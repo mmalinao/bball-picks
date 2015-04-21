316b162f6129297715216fc47efbf56d09442a75
mmalinao <matt.malinao@gmail.com>
2015-04-20 23:13:00 -0600

    add SportRadarApi http client to sport radar api; get schedules endpoint

---
+++ lib/sport_radar_api.rb
@@ -0,0 +1,13 @@
+class SportRadarApi
+  include HTTParty
+
+  base_uri 'api.sportradar.us'
+  default_params output: 'json'
+  format :json
+
+  OPTIONS = { query: { api_key: ENV['SPORTRADAR_API_KEY'] } }
+
+  def self.schedules
+    (get '/nba-t3/series/2014/PST/schedule.json', OPTIONS).parsed_response.with_indifferent_access
+  end
+end
