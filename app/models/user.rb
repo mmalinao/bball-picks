f67a23fa66c12be32ff2d4c15dffc7cc1fcec7b1
mmalinao <matt.malinao@gmail.com>
2015-04-24 21:20:53 -0600

    add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  enum role: { regular: 0, admin: 1 }
+
   has_many :picks, class_name: 'Player', foreign_key: 'fantasy_draft_manager_id'
 
   validates :user_name, presence: true
